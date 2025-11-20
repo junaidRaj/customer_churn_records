ef8bc02cd686a8f3e9596e27374778d23c2ab494
junaidRaj <junaidbaba781@gmail.com>
2025-11-20 18:46:10 +0500

    added chart

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 df = pd.read_csv('./Customer-Churn-Records.csv')
 
 count_exited = df['Exited'].value_counts(normalize=True).multiply(100).round(2)
-
-# make pivot table find balance exited customers based on geography
 
 balance_churn_pivot = df.pivot_table(
     values="Balance",
@@ -12,10 +13,32 @@ balance_churn_pivot = df.pivot_table(
     aggfunc='mean'
 )
 
-# NumberOfProducts exited customers count average
 Product_churn_rate = df.groupby('NumOfProducts')['Exited'].mean().mul(100).round(2)
 
 age_bins = pd.cut(df['Age'], bins=4)
 
 age_churn_rate = df.groupby(age_bins)['Exited'].mean().mul(100).round(2)
-print("Churn Rate by Age Groups:",age_churn_rate)
+
+satisfaction_pivot = df.pivot_table(
+    values="Satisfaction Score",
+    index="Exited",
+    aggfunc='mean'
+)
+
+products_churn_rate_df = df.groupby('NumOfProducts')['Exited'].mean().mul(100).round(2).reset_index()
+
+plt.figure(figsize=(8, 5))
+
+sns.barplot(
+    x='NumOfProducts',
+    y='Exited',
+    data=products_churn_rate_df,
+    palette=['green', 'blue', 'red', 'darkred']
+)
+
+plt.title('Operational Risk: Churn Rate by Number of Products', fontsize=14)
+plt.ylabel('Churn Rate (%)', fontsize=12)
+plt.xlabel('Number of Products Held by Customer', fontsize=12)
+plt.yticks(np.arange(0, 101, 20))
+plt.grid(axis='y', linestyle='--', alpha=0.5)
+plt.show()
